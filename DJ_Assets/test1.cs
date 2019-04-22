2524d58dec42d0da2aae0348e8c00069ab147786
Doug <doug530johnson@gmail.com>
2019-04-21 22:17:45 -0700

    Add files via upload

---
+++ DJ_Assets/test1.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+using UnityEngine.SceneManagement;
+
+namespace Tests
+{
+    public class test1
+    {
+        // A Test behaves as an ordinary method
+        [Test]
+        public void TestLoadSceneSimplePasses()
+        {
+            // Use the Assert class to test conditions
+        }
+
+        // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
+        // `yield return null;` to skip a frame.
+        [UnityTest]
+        public IEnumerator TestLoadSceneWithEnumeratorPasses()
+        {
+            SceneManager.LoadScene("DK2");
+                yield return new WaitForSeconds(3);
+            // Load states into player states?
+            Assert.AreEqual(SceneManager.GetActiveScene().name, "DK2");
+            SceneManager.LoadScene("End");
+            yield return new WaitForSeconds(3);
+        }
+    }
+}
